99cae884b1b68193d5dd22302c9fcdacc4992146
Vesel Atanasov <77515052+VeselAtanasov@users.noreply.github.com>
2021-01-29 22:52:37 +0200

    Initial commit

--- src/ConditionalStatementsAdvancedExercise/OnTimeForTheExam08.java
+++ src/ConditionalStatementsAdvancedExercise/OnTimeForTheExam08.java
@@ -7,7 +7,7 @@ public class OnTimeForTheExam08 {
         Scanner scanner = new Scanner(System.in);
 
         int examHour = Integer.parseInt(scanner.nextLine());
-        int examMinute  = Integer.parseInt(scanner.nextLine());
+        int examMinute = Integer.parseInt(scanner.nextLine());
         int arriveHour = Integer.parseInt(scanner.nextLine());
         int arriveMinute = Integer.parseInt(scanner.nextLine());
 
@@ -15,8 +15,41 @@ public class OnTimeForTheExam08 {
 
         int examTime = examHour * 60 + examMinute;
         int arrivedTime = arriveHour * 60 + arriveMinute;
+        String output = "";
+        String output2 = "";
+        int difference = 0;
 
+        if (arrivedTime < examMinute - 30) {
+            output = "Early";
+            difference = examTime - arrivedTime;
 
+            if (difference < 60) {
+                output2 = String.format("%d minutes before the start", difference);
+            } else {
+                int hour = difference / 60;
+                int minutes = difference % 60;
+                output2 = String.format("%d:%02d hours before the start", hour, minutes);
+            }
+        } else if (arrivedTime < examTime) {
+            output = "On time";
+            difference = examTime - arrivedTime;
+            output2 = String.format("%d minutes before the start", difference);
 
+        } else if (arrivedTime > examTime) {
+
+            output = "Late";
+            difference = arrivedTime - examTime;
+            if (difference < 60) {
+                output2 = String.format("%d minutes after the start", difference);
+            } else {
+                int hour = difference / 60;
+                int minutes = difference % 60;
+                output2 = String.format("%d:%02d hours after the start", hour, minutes);
+            }
+        }
+
+        System.out.println(output);
+        System.out.println(output2);
     }
 }
+
