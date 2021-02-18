892432d671e342ed7dcdf7bdb1ee725d250215f2
Vesel Atanasov <77515052+VeselAtanasov@users.noreply.github.com>
2021-02-18 22:58:48 +0200

    Initial commit

---
+++ src/07ProgrammingBasicsOnlineExam/BestPlayer05.java
@@ -0,0 +1,32 @@
+package ProgrammingBasicsOnlineExam;
+
+import java.util.Scanner;
+
+public class BestPlayer05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int goalsMax = 0;
+        String best = "";
+
+        while (!name.equals("END")) {
+            int goals = Integer.parseInt(scanner.nextLine());
+            if (goals > goalsMax) {
+                best = name;
+                goalsMax = goals;
+            }
+            if (goals >= 10) {
+                break;
+            }
+            name = scanner.nextLine();
+        }
+        System.out.printf("%s is the best player!%n", best);
+
+        if (goalsMax < 3) {
+            System.out.printf("He has scored %d goals.", goalsMax);
+        } else {
+            System.out.printf("He has scored %d goals and made a hat-trick !!!", goalsMax);
+        }
+    }
+}
---
+++ src/07ProgrammingBasicsOnlineExam/TournamentOfChristmas06.java
@@ -0,0 +1,51 @@
+package ProgrammingBasicsOnlineExam;
+
+import java.util.Scanner;
+
+public class TournamentOfChristmas06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = Integer.parseInt(scanner.nextLine());
+        int allWins = 0;
+        int allLoses = 0;
+        double allMoney = 0;
+        int counterWin = 0;
+        int counterLoose = 0;
+        double dailyMoney = 0;
+
+        for (int i = 1; i <= days; i++) {
+
+            while (true) {
+                String input = scanner.nextLine();
+                if (input.equals("Finish")) {
+                    if (counterWin > counterLoose) {
+                        dailyMoney *= 1.1;
+                        allWins++;
+
+                    } else if (counterWin < counterLoose && counterLoose != 0) {
+                        allLoses++;
+                    }
+                    allMoney += dailyMoney;
+                    counterWin = 0;
+                    counterLoose = 0;
+                    dailyMoney = 0;
+                    break;
+                }
+                String result = scanner.nextLine();
+                if (result.equals("win")) {
+                    dailyMoney += 20;
+                    counterWin++;
+                } else if (result.equals("lose")) {
+                    counterLoose++;
+                }
+            }
+        }
+        if (allWins > allLoses) {
+            allMoney *= 1.2;
+            System.out.printf("You won the tournament! Total raised money: %.2f", allMoney);
+        } else {
+            System.out.printf("You lost the tournament! Total raised money: %.2f", allMoney);
+        }
+    }
+}
