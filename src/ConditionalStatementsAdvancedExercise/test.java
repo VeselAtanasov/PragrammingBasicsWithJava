a061076be97939859b1a39340feb1d70b0923999
Vesel Atanasov <77515052+VeselAtanasov@users.noreply.github.com>
2021-03-21 20:27:09 +0200

    Initial commit

---
+++ src/ConditionalStatementsAdvancedExercise/test.java
@@ -0,0 +1,10 @@
+package ConditionalStatementsAdvancedExercise;
+
+public class test {
+    public static void main(String[] args) {
+
+        int a = 8/2*(2+2);
+
+        System.out.print(a);
+    }
+}
